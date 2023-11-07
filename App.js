3b490c6c698cdf86acd9861e6a068269b0f96b23
AbhayParasharhere <83302622+AbhayParasharhere@users.noreply.github.com>
2023-11-06 21:05:53 -0800

    Add basic layout

---
+++ App.js
@@ -0,0 +1,16 @@
+import { View, StyleSheet } from "react-native";
+import CurrentWeather from "./src/components/CurrentWeather";
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <CurrentWeather />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+});
